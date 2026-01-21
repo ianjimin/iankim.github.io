70c04029dafb8bf76daf26967a869341acff3abe
Ian <bigthirty@Ians-MacBook-Pro-2.local>
2026-01-21 14:46:43 -0500

    Fix ThemeProvider import - use default export

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Projects from './components/Projects'
 import Services from './components/Services'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
-import { ThemeProvider } from './context/ThemeContext'
+import ThemeProvider from './context/ThemeContext'
 
 function App() {
   return (
